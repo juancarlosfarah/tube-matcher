647c917960f6f5225264f6c30def3b7e689129fd
Juan Carlos Farah <farah.juancarlos@gmail.com>
2014-10-14 22:57:20 +0100

    Attempt to fix problem with makefile and test.

--- makefile
+++ makefile
@@ -1,5 +1,6 @@
-tubematch.o: main.cpp match.cpp deduplicator.cpp
-	g++ -g main.cpp match.cpp deduplicator.cpp -o tubematch.o
+tubematch.o: main.cpp match.cpp deduplicator.cpp test_deduplicator.cpp
+	g++ -Wall -g main.cpp match.cpp deduplicator.cpp -o tubematch.o
 
-test_deduplicator.o: test_deduplicator.cpp deduplicator.cpp
-	g++ -g test_deduplicator.cpp deduplicator.cpp -o test_deduplicator.o
+#TODO: Fix this rule.
+#test_deduplicator.o: test_deduplicator.cpp deduplicator.cpp
+#	g++ -Wall -g test_deduplicator.cpp deduplicator.cpp -o test_deduplicator.o
--- test_deduplicator.cpp
+++ test_deduplicator.cpp
@@ -5,19 +5,19 @@
 using namespace std;
 
 int main() {
-	cout << "Testing function deduplicate():" << endl;
+	cout << "Testing function Deduplicate():" << endl;
 
 	/* Test 1: "Juan Carlos Farah" */
 	string s1 = "Juan Carlos Farah";
 	cout << "Test 1: " << s1 << endl;
 	cout << "Expected Result: " << "juan crlosfh" << endl;
-	cout << "Actual Result: " << deduplicate(s1) << endl;
+	cout << "Actual Result: " << Deduplicate(s1) << endl;
 
 	/* Test 2: "Christos Kaplanis" */
 	string s2 = "Christos Kaplanis";
 	cout << "Test 2: " << s2 << endl;
 	cout << "Expected Result: " << "christo kapln" << endl;
-	cout << "Actual Result: " << deduplicate(s2) << endl;
+	cout << "Actual Result: " << Deduplicate(s2) << endl;
 
 	return 0;
 
